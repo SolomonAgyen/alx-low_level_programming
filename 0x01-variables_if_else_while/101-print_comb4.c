3cf1e2f123862a504f22670da331ea0e9b1681e8
SolomonAgyen <agyensolomon1@gmail.com>
2023-08-11 19:22:00 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  * Description: Prints all possible different combinations of three digits
- * separated by ,, followed by a space.
+ *              separated by ,, followed by a space.
  *
  * Return: Always 0 (Success)
  */
@@ -12,16 +12,19 @@ int main(void)
 int first_digit;
 int second_digit;
 int third_digit;
-for (first_digit = 0; first_digit <= 9; first_digit++)
+for (first_digit = 0; first_digit <= 7; first_digit++)
 {
-for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+for (second_digit = first_digit + 1; second_digit <= 8; second_digit++)
 {
 for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
 {
-printf(("%d%d%d", first_digit, second_digit, third_digit);
-if (first_digit != 7 &&  second_digit != 8 &&  third_digit != 9)
+putchar(first_digit + '0');
+putchar(second_digit + '0');
+putchar(third_digit + '0');
+if (first_digit != 7  second_digit != 8  third_digit != 9)
 {
-printf(", ");
+putchar(',');
+putchar(' ');
 }
 }
 }
