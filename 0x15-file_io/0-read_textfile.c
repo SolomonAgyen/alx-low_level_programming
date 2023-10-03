31972e1cadd70db70f9e0a5e92513f444096a20b
SolomonAgyen <SolomonAgyen>
2023-10-03 02:14:45 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,12 +8,12 @@
  * Return: w- actual number of bytes read and printed
  * 0 when function fails or filename is NULL.
  */
-int read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
-ssize_t fd;
+int fd;
 ssize_t w;
-ssize_t t;
+int t;
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
